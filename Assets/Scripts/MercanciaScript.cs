1b142f110a8280a1b37f8f6a01840f145f137a3f
ZahirFara <167230564+ZahirFara@users.noreply.github.com>
2025-05-30 09:08:07 -0300

    30/05

---
+++ Assets/Scripts/MercanciaScript.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MercanciaScript : MonoBehaviour
+{
+    public int precio = 10;
+    public DineroManager DineroManager;
+
+     void Start()
+    {
+        DineroManager = FindObjectOfType<DineroManager>();
+    }
+
+    void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            DineroManager.UpdateMoney(-precio);
+            Destroy(gameObject);
+        }
+    }
+}
