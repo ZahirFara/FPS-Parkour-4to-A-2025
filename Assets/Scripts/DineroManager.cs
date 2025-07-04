d00779d190dcc1eca34955cc3c3c4564515c515d
ZahirFara <167230564+ZahirFara@users.noreply.github.com>
2025-07-04 09:08:50 -0300

    tp final principio

--- Assets/Scripts/DineroManager.cs
+++ Assets/Scripts/DineroManager.cs
@@ -5,34 +5,20 @@ using UnityEngine;
 public class DineroManager : MonoBehaviour
 {
     public float playerMoney;
-<<<<<<< HEAD
     public UIManager uiManager;
-}
+
     public void UpdateMoney(float amount)
-    {
+    { 
 
         if (playerMoney + amount < 0)
         {
-            //impedir compra
-=======
-
-    public void UpdateMoney(float amount)
-    {
-   
-        if (playerMoney + amount < 0)
-        {
-
->>>>>>> adeea3542b19aa28b8ac0a1cd76caa4d260ac2ac
+          
         }
         else
         {
             playerMoney += amount;
-<<<<<<< HEAD
-        UIManager.UpdateMoneyUI(playermoney)
+            uiManager.UpdateMoneyUI(playerMoney.ToString());
+         
         }
-=======
-        }
-   
->>>>>>> adeea3542b19aa28b8ac0a1cd76caa4d260ac2ac
     }
 }
--- Assets/Scripts/UIManager.cs
+++ Assets/Scripts/UIManager.cs
@@ -1,34 +1,19 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-<<<<<<< HEAD
 using TMPro;
 
 public class UIManager : MonoBehaviour
 {
+    public TextMeshProUGUI txtMoney;
 
-    public TextMeshProUGUItxtMonet;
-=======
-
-public class UIManager : MonoBehaviour
-{
->>>>>>> adeea3542b19aa28b8ac0a1cd76caa4d260ac2ac
-    // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
-<<<<<<< HEAD
-  
-   public void UpdateMoneyUI(string moneyamount)
+    public void UpdateMoneyUI(string moneyAmount)
     {
-        txtMone.text
-=======
-    // Update is called once per frame
-    void Update()
-    {
-        
->>>>>>> adeea3542b19aa28b8ac0a1cd76caa4d260ac2ac
+        txtMoney.text = moneyAmount;
     }
 }
