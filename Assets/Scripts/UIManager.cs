a927831768a63c875372eacc19e01a345bf7ce16
FARA ZAHIR <zahir@maserati.com.ar>
2025-07-14 14:27:51 -0300

    nkjn

--- Assets/Scripts/UIManager.cs
+++ Assets/Scripts/UIManager.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
 
-public class UIController : MonoBehaviour
+public class UIManager: MonoBehaviour
 {
     public TMP_Text Money;
     public DineroManager dineroManager;
